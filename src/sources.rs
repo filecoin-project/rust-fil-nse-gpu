cef86f6eac8136a643d00084aff333f3c7aaa971
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-04-30 00:32:32 +0430

    feat: combine codes with itertools

--- src/sources.rs
+++ src/sources.rs
@@ -1,7 +1,8 @@
 use ff::PrimeField;
+use itertools::join;
 
 static NSE_SRC: &str = include_str!("cl/nse.cl");
 
 pub fn generate_nse_program<F: PrimeField>() -> String {
-    format!("{}\n{}", ff_cl_gen::field::<F>("F"), NSE_SRC.to_string())
+    join(&[ff_cl_gen::field::<F>("F"), NSE_SRC.to_string()], "\n")
 }
