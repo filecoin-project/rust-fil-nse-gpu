f1989b553395b6f996131e69c59c5b7274624719
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-04-30 00:35:15 +0430

    feat: add sha256 source file

--- src/sources.rs
+++ src/sources.rs
@@ -2,7 +2,15 @@ use ff::PrimeField;
 use itertools::join;
 
 static NSE_SRC: &str = include_str!("cl/nse.cl");
+static SHA256_SRC: &str = include_str!("cl/hash/sha256.cl");
 
 pub fn generate_nse_program<F: PrimeField>() -> String {
-    join(&[ff_cl_gen::field::<F>("F"), NSE_SRC.to_string()], "\n")
+    join(
+        &[
+            ff_cl_gen::field::<F>("F"),
+            SHA256_SRC.to_string(),
+            NSE_SRC.to_string(),
+        ],
+        "\n",
+    )
 }
