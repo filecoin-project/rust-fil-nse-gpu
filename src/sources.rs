3face43725f803b63857174361373414890285b6
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-04-30 00:38:01 +0430

    refact: add field name constant

--- src/sources.rs
+++ src/sources.rs
@@ -5,9 +5,10 @@ static NSE_SRC: &str = include_str!("cl/nse.cl");
 static SHA256_SRC: &str = include_str!("cl/hash/sha256.cl");
 
 pub fn generate_nse_program<F: PrimeField>() -> String {
+    const FIELD_NAME: &str = "Field";
     join(
         &[
-            ff_cl_gen::field::<F>("F"),
+            ff_cl_gen::field::<F>(FIELD_NAME),
             SHA256_SRC.to_string(),
             NSE_SRC.to_string(),
         ],
