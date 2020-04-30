cf452e87e19c71ce7c593e85d2f34f488b6e7ac1
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-05-01 01:43:49 +0430

    feat: add nse interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,14 @@
 mod sources;
+
+use paired::bls12_381::Fr;
+
+pub struct Layer {
+    nodes: Vec<Fr>,
+}
+
+pub trait NarrowStackedExpander {
+    fn generate_mask_layer(&mut self, replica_id: Fr, window_index: usize) -> Layer;
+    fn generate_expander_layer(&mut self, layer_index: usize) -> Layer;
+    fn generate_butterfly_layer(&mut self, layer_index: usize) -> Layer;
+    fn combine_segment(&self, offset: usize, segment: &mut [Fr]);
+}
