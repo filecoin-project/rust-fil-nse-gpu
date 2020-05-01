69a6c4662cf18b1490c439a039cd686aa9e37988
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-05-01 22:23:42 +0430

    feat: NSE field is set to Bls12-381

--- src/sources.rs
+++ src/sources.rs
@@ -1,5 +1,5 @@
-use ff::PrimeField;
 use itertools::join;
+use paired::bls12_381::Fr;
 
 #[allow(dead_code)]
 static NSE_SRC: &str = include_str!("cl/nse.cl");
@@ -7,11 +7,10 @@ static NSE_SRC: &str = include_str!("cl/nse.cl");
 static SHA256_SRC: &str = include_str!("cl/hash/sha256.cl");
 
 #[allow(dead_code)]
-pub fn generate_nse_program<F: PrimeField>() -> String {
-    const FIELD_NAME: &str = "Field";
+pub fn generate_nse_program() -> String {
     join(
         &[
-            ff_cl_gen::field::<F>(FIELD_NAME),
+            ff_cl_gen::field::<Fr>("Fr"),
             SHA256_SRC.to_string(),
             NSE_SRC.to_string(),
         ],
