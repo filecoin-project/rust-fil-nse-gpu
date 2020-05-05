9cc8379e595498420fdde36d1b34c29af1da434e
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-05-06 02:51:10 +0430

    feat: add bit stream and expander graph functions

--- src/sources.rs
+++ src/sources.rs
@@ -2,6 +2,7 @@ use itertools::join;
 use paired::bls12_381::Fr;
 
 static SHA256_SRC: &str = include_str!("cl/hash/sha256.cl");
+static COMMON_SRC: &str = include_str!("cl/common.cl");
 static MASK_SRC: &str = include_str!("cl/mask.cl");
 static EXPANDER_SRC: &str = include_str!("cl/expander.cl");
 static BUTTERFLY_SRC: &str = include_str!("cl/butterfly.cl");
@@ -12,6 +13,7 @@ pub fn generate_nse_program() -> String {
         &[
             ff_cl_gen::field::<Fr>("Fr"),
             SHA256_SRC.to_string(),
+            COMMON_SRC.to_string(),
             MASK_SRC.to_string(),
             EXPANDER_SRC.to_string(),
             BUTTERFLY_SRC.to_string(),
