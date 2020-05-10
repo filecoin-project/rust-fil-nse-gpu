c9178bc2422ef1fa332a2f7b5859073e6fa3e5e4
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-05-10 15:52:23 +0430

    feat: CLI program for benchmarking

--- src/bin/bench.rs
+++ src/bin/bench.rs
@@ -1,30 +1,112 @@
 use rand::{thread_rng, Rng};
 use rust_fil_nse_gpu::*;
 use std::time::Instant;
+use structopt::StructOpt;
+
+macro_rules! timer {
+    ($e:expr, $samples:expr) => {{
+        let before = Instant::now();
+        for _ in 0..$samples {
+            $e;
+        }
+        (before.elapsed().as_secs() * 1000 as u64 + before.elapsed().subsec_millis() as u64)
+            / ($samples as u64)
+    }};
+}
+
+macro_rules! bench_mask {
+    ($conf:expr, $samples:expr) => {{
+        let mut rng = thread_rng();
+        let replica_id = Sha256Domain::random(&mut rng);
+        let window_index: usize = rng.gen();
+        let mut gpu = GPU::new($conf).unwrap();
+        timer!(
+            gpu.generate_mask_layer(replica_id, window_index).unwrap(),
+            $samples
+        )
+    }};
+}
+
+macro_rules! bench_expander {
+    ($conf:expr, $samples:expr) => {{
+        let mut rng = thread_rng();
+        let replica_id = Sha256Domain::random(&mut rng);
+        let window_index: usize = rng.gen();
+        let mut gpu = GPU::new($conf).unwrap();
+        gpu.generate_mask_layer(replica_id, window_index).unwrap();
+        timer!(
+            gpu.generate_expander_layer(replica_id, window_index, rng.gen())
+                .unwrap(),
+            $samples
+        )
+    }};
+}
+
+macro_rules! bench_butterfly {
+    ($conf:expr, $samples:expr) => {{
+        let mut rng = thread_rng();
+        let replica_id = Sha256Domain::random(&mut rng);
+        let window_index: usize = rng.gen();
+        let mut gpu = GPU::new($conf).unwrap();
+        gpu.generate_mask_layer(replica_id, window_index).unwrap();
+        timer!(
+            gpu.generate_butterfly_layer(replica_id, window_index, rng.gen())
+                .unwrap(),
+            $samples
+        )
+    }};
+}
+
+macro_rules! bench_combine {
+    ($conf:expr, $samples:expr) => {{
+        let mut rng = thread_rng();
+        let replica_id = Sha256Domain::random(&mut rng);
+        let window_index: usize = rng.gen();
+        let data = Layer::random(&mut rng, $conf.num_nodes_window);
+        let mut gpu = GPU::new($conf).unwrap();
+        gpu.generate_mask_layer(replica_id, window_index).unwrap();
+        timer!(gpu.combine_layer(&data, false).unwrap(), $samples)
+    }};
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "NSE Config", about = "CLI configuration of NSE.")]
+struct ConfigCLI {
+    #[structopt(short = "k", default_value = "8")]
+    k: u32,
+    #[structopt(long = "num-nodes-window", default_value = "524288")]
+    num_nodes_window: usize,
+    #[structopt(long = "degree-expander", default_value = "384")]
+    degree_expander: usize,
+    #[structopt(long = "degree-butterfly", default_value = "16")]
+    degree_butterfly: usize,
+    #[structopt(long = "num-expander-layers", default_value = "8")]
+    num_expander_layers: usize,
+    #[structopt(long = "num-butterfly-layers", default_value = "7")]
+    num_butterfly_layers: usize,
+}
+
+impl From<ConfigCLI> for Config {
+    fn from(cli: ConfigCLI) -> Self {
+        Config {
+            k: cli.k,
+            num_nodes_window: cli.num_nodes_window,
+            degree_expander: cli.degree_expander,
+            degree_butterfly: cli.degree_butterfly,
+            num_expander_layers: cli.num_expander_layers,
+            num_butterfly_layers: cli.num_butterfly_layers,
+        }
+    }
+}
 
 fn main() {
-    let config = Config {
-        k: 8,
-        num_nodes_window: 1 << 27,
-        degree_expander: 384,
-        degree_butterfly: 16,
-        num_expander_layers: 8,
-        num_butterfly_layers: 7,
-    };
+    const SAMPLES: usize = 4;
+    let config: Config = Config::from(ConfigCLI::from_args());
 
-    let mut rng = thread_rng();
-    let original_data = Layer::random(&mut rng, config.num_nodes_window);
-    let replica_id = Sha256Domain::random(&mut rng);
-    let window_index: usize = rng.gen();
+    println!("Config: {:?}", config);
 
-    let gpu = GPU::new(config).unwrap();
-    let sealer = Sealer::new(config, replica_id, window_index, original_data, gpu).unwrap();
-
-    let mut before = Instant::now();
-    for _ in sealer {
-        let dur =
-            before.elapsed().as_secs() * 1000 as u64 + before.elapsed().subsec_millis() as u64;
-        println!("Layer took: {}ms", dur);
-        before = Instant::now();
-    }
+    println!("Mask: {}ms", bench_mask!(config, SAMPLES));
+    println!("Expander: {}ms", bench_expander!(config, SAMPLES));
+    println!("Butterfly: {}ms", bench_butterfly!(config, SAMPLES));
+    println!("Combine: {}ms", bench_combine!(config, SAMPLES));
 }
